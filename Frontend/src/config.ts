e560c3bbe596baba1b6a1c559f7ef8fd5c45ea36
Azan Adnan <azanbutt2311@gmail.com>
2024-07-11 20:24:31 +0500

    update

---
+++ Frontend/src/config.ts
@@ -0,0 +1,23 @@
+// import { AlchemyGasManagerConfig } from "@alchemy/aa-alchemy";
+// import {
+//   SupportedAccountTypes,
+//   cookieStorage,
+//   createConfig,
+// } from "@alchemy/aa-alchemy/config";
+
+
+// import {arbitrumSepolia} from "@alchemy/aa-core";
+
+
+// export const chain = arbitrumSepolia;
+// export const config = createConfig({
+//   // this is for requests to the specific chain RPC
+//   rpcUrl: "/api/rpc/chain/" + chain.id,
+//   signerConnection: {
+//     // this is for Alchemy Signer requests
+//     rpcUrl: "/api/rpc/",
+//   },
+//   chain,
+//   //ssr: true,
+//   storage: cookieStorage,
+// });
