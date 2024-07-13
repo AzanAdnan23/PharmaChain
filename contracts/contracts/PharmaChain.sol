bc3e04b15b333a1adac22ed09c7da57e453b2315
Azan Adnan <azanbutt2311@gmail.com>
2024-07-13 19:02:11 +0500

    .

--- contracts/contracts/PharmaChain.sol
+++ contracts/contracts/PharmaChain.sol
@@ -6,4 +6,7 @@ import "hardhat/console.sol";
 contract PharmaChain {
     // will check from this agr user ha then authenticate. if not then create a new account wali screen
     mapping(address => bool) users;
+
+    // main/master manufacturer accounts
+    // sub accounts for manufacturers
 }
---
+++ frontend/src/pages/landingpage.tsx
@@ -0,0 +1,2 @@
+// landing page b bnana sexy sa
+// https://www.inven.ai/company-lists/top-27-blockchain-supply-chain-companies
