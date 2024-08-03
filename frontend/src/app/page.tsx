84d09fee10319c44b00d0df19764bf545aee82d6
Muhammad Shahwaiz <mshahwaiz12@gmail.com>
2024-08-03 05:21:20 +0500

    refactor: Remove unused components and update routing in page.tsx

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,13 +1,17 @@
 "use client";
 
-import Navbar from "@/components/Navbar";
-import LandingPage from "@/components/LandingPage";
+import { useRouter } from "next/navigation"
 
 export default function Home() {
 
+  const router = useRouter();
+
+  router.push("/login");
+
   return (
-    <div>
-      <LandingPage />
-    </div>
+    // <div>
+    //   <LandingPage />
+    // </div>
+    <></>
   );
 }
