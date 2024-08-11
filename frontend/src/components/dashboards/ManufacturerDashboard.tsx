a1ec65cd7b92c8ac1c102ae7b90619ebaa6ede10
Muhammad Shahwaiz <mshahwaiz12@gmail.com>
2024-08-11 17:15:27 +0500

    refactor: Add Outgoing RFID button to ManufacturerDashboard

--- frontend/src/components/dashboards/ManufacturerDashboard.tsx
+++ frontend/src/components/dashboards/ManufacturerDashboard.tsx
@@ -7,9 +7,15 @@ import CurrentOrdersTable from "../manufacturer/CurrentOrders";
 import OrdersFulfilledTable from "../manufacturer/OrdersFulfilled";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { BChart } from "@/components/charts/BChart"
+import { Button } from "@/components/ui/button"
 
 
 const ManufacturerDashboard = () => {
+
+  const OutgoingRFID = () => {
+    console.log("Outgoing RFID stuff");
+  }
+
   return (
     <main className="p-4 grid grid-cols-4 gap-4 flex-grow">
       <div className="col-span-1 flex flex-col gap-4">
@@ -17,6 +23,7 @@ const ManufacturerDashboard = () => {
           <CreateBatchForm />
         </div>
         <BChart chartTitle="Popular Orders" chartDescription="" />
+        <Button onClick={OutgoingRFID} className="w-full">Outgoing RFID</Button>
       </div>
       <div className="col-span-3">
         <Tabs defaultValue="createdbatches" className="h-full flex flex-col">
